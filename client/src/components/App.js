83944ea98e36a9234e639dac3e6fb0e228e82fd0
nottea15 <th3fr3eze@gmail.com>
2022-08-13 01:26:13 +0300

    Google auth with @react-oauth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+import Header from "./Header";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 const App = () => {
-  return <div className="ui container">App</div>;
+  return (
+    <GoogleOAuthProvider clientId="243052285267-01f7ndaqfitc2m7isfee2phd37mnlbam.apps.googleusercontent.com">
+      <div className="ui container">
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<StreamList />} />
+            <Route path="/streams/create" element={<StreamCreate />} />
+            <Route path="/streams/edit" element={<StreamEdit />} />
+            <Route path="/streams/delete" element={<StreamDelete />} />
+            <Route path="/streams/show" element={<StreamShow />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </GoogleOAuthProvider>
+  );
 };
 
 export default App;
+
+//243052285267-0pr0fvhgrjmjmdfehb7389ahnkvpaunk.apps.googleusercontent.com
