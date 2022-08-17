150530cd1a6e4350338d550584fd7d0e8a7c3e77
nottea15 <th3fr3eze@gmail.com>
2022-08-17 15:06:06 +0300

    final stream realization

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { fetchStream } from "../../actions";
+import flv from "flv.js";
 
 const StreamShow = () => {
-  return <div>StreamShow</div>;
+  const id = useParams();
+  const videoRef = useRef();
+  let player = null;
+
+  const buidPlayer = () => {
+    if (player || !stream) {
+      return;
+    }
+
+    player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id.id}.flv`,
+    });
+    player.attachMediaElement(videoRef.current);
+    player.load();
+  };
+
+  useEffect(() => {
+    dispatch(fetchStream(id.id));
+    buidPlayer();
+
+    return () => {
+      console.log("unmount");
+      player.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    buidPlayer();
+  });
+
+  const dispatch = useDispatch();
+  const stream = useSelector((state) => state.streams[id.id]);
+  if (!stream) {
+    return <div className="ui active loader"></div>;
+  }
+  return (
+    <div>
+      <video ref={videoRef} style={{ width: "100%" }} controls />
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </div>
+  );
 };
 
 export default StreamShow;
